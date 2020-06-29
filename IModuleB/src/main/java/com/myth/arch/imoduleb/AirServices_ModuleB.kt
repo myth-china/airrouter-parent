eeedcfaad86a88307fe33328706c2e6bc6949c40
yanweiqiang04496 <yanweiqiang04496@hellobike.com>
2020-06-29 10:39:39 +0800

    Add repository snapshot url

--- IModuleB/src/main/java/com/myth/arch/imoduleb/AirServices_ModuleB.kt
+++ IModuleB/src/main/java/com/myth/arch/imoduleb/AirServices_ModuleB.kt
@@ -3,6 +3,9 @@ package com.myth.arch.imoduleb
 import com.myth.arch.airrouter.AirRouter.getService
 
 object AirServices_ModuleB {
+  fun getModuleBRouter(): IModuleBRouter = getService("com.myth.arch.moduleb.ModuleBRouter") as
+      com.myth.arch.imoduleb.IModuleBRouter
+
   fun getModuleBServiceImpl3(): IModuleBService =
       getService("com.myth.arch.moduleb.ModuleBServiceImpl3") as
       com.myth.arch.imoduleb.IModuleBService
@@ -18,7 +21,4 @@ object AirServices_ModuleB {
   fun getModuleBServiceImpl4(): IModuleBService =
       getService("com.myth.arch.moduleb.ModuleBServiceImpl4") as
       com.myth.arch.imoduleb.IModuleBService
-
-  fun getModuleBRouter(): IModuleBRouter = getService("com.myth.arch.moduleb.ModuleBRouter") as
-      com.myth.arch.imoduleb.IModuleBRouter
 }
